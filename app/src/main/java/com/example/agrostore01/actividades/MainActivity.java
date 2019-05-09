3f8f3d6f26c6793585b06399fcf4734195051646
maryfer.jaquezkimlee@gmail.com <correo@ejemplo.com>
2019-05-08 19:39:25 -0500

    en contruccion pero es un avance

--- app/src/main/java/com/example/agrostore01/actividades/MainActivity.java
+++ app/src/main/java/com/example/agrostore01/actividades/MainActivity.java
@@ -1,19 +1,66 @@
 package com.example.agrostore01.actividades;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.agrostore01.R;
+import com.example.agrostore01.fragmentos.BuscarFragment;
+import com.example.agrostore01.fragmentos.CarritoFragment;
+import com.example.agrostore01.fragmentos.DashboardFragment;
+import com.example.agrostore01.fragmentos.NotificacionesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    // ahista
+    private BottomNavigationView navegador;
+
+    private DashboardFragment dashboardFragment = new DashboardFragment();
+    private BuscarFragment buscarFragment = new BuscarFragment();
+    private CarritoFragment carritoFragment = new CarritoFragment();
+    private NotificacionesFragment notificacionesFragment = new NotificacionesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navegador = findViewById(R.id.navigation);
+        navegador.setOnNavigationItemSelectedListener(navegadorListener);
+
+        // fragmento for defecto
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, dashboardFragment).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navegadorListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragmento = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_home:
+                    fragmento = dashboardFragment;
+                    break;
+                case R.id.navigation_buscar:
+                    fragmento = buscarFragment;
+                    break;
+                case R.id.navigation_carrito:
+                    fragmento = carritoFragment;
+                    break;
+                case R.id.navigation_notificaciones:
+                    fragmento = notificacionesFragment;
+                    break;
+            }
+
+            if (fragmento == null)
+                return false;
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragmento).commit();
+
+            return true;
+        }
+    };
 
 }
---
+++ app/src/main/java/com/example/agrostore01/fragmentos/CarritoFragment.java
@@ -0,0 +1,20 @@
+package com.example.agrostore01.fragmentos;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.agrostore01.R;
+
+public class CarritoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_carrito, container, false);
+        return vista;
+    }
+}
